6c97047a0e49b1165ad051640ce827b22680bd13
dmitriy kim <d.villarionovich@gmail.com>
2024-08-28 00:31:29 +0200

    solution

--- scrape_app/data_app/custom_stop.py
+++ scrape_app/data_app/custom_stop.py
@@ -1,2 +1,17 @@
-
-data = "asd"
+custom_stop_words = [
+    "experience", "team", "work", "development", "knowledge", "working", "and", "of", "the", "with",
+    "to", "in", "will", "data", "•", "software", "understanding", "з", "в", "та", "досвід", "роботи", "на",
+    "company", "strong", "code", "—", "skills", "support", "product", "new", "years", "using", "для", "english",
+    "ability", "🔹", "paid", "days", "professional", "join", "best", "що", "including", "/", "communication",
+    "developing", "looking", "business", "flexible", "до", "familiarity", "*", "ensure", "responsibilities:",
+    "part", "interview", "customer", "existing", "requirements:", "&", "за", "good", "level", "complex", "teams",
+    "learning", "requirements", "знання", "performance", "us", "opportunity", "compensation", "offer:", "we’re",
+    "excellent", "regular", "practices", "vacation", "collaborate", "like", "principles",
+    "technical", "у", "ми", "services", "web", "design", "solutions", "і", "backend",
+    "systems", "develop", "implement", "tools", "developer", "platform", "project", "maintain", "quality",
+    "proficiency", "sick", "language", "provide", "engineer", "management", "projects", "automation", "applications", "remote", "technologies", "programming", "test", "engineering", "environment", "innovative", "etc.)", "frameworks", "corporate", "system", "within", "help", "team.", "building",
+    "expertise", "basic", "build", "internal", "client", "improve", "least", "leave", "create", "computer",
+    "scalable", "medical", "one", "ensuring", "technology", "seeking", "infrastructure", "solutions.", "global",
+    "devops", "issues", "даних", "tech", "related", "lead","features", "offer", "time", "great",
+    "google", "security", "office", "providing", "design,", "tasks", "insurance", "health", "framework", "various", "experienced", "role", "integration", "skills:", "від", "hands-on", "responsibilities","etc", "plus", "5", "3", "clients", "customers", "apis", "architecture", "2"
+]
